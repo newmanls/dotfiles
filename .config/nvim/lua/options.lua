e6f964b7eff0c069b2e53e85dcb653502edeaf27
lr-tech <lrtech8@gmail.com>
2022-04-26 00:21:06 -0400

    Fix indentation. Remove custom keymappings for terminal movement. Remove scrolloff plugin and using builtin scrolloff instead

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,31 +1,29 @@
 -- Interface
-vim.opt.termguicolors = true
-vim.opt.syntax = 'on'
+vim.opt.colorcolumn = '80'
+vim.opt.cursorline = true
+vim.opt.laststatus = 1
 vim.opt.lazyredraw = true
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.cursorline = true
-vim.opt.colorcolumn = '80'
+vim.opt.syntax = 'on'
+vim.opt.termguicolors = true
 
--- Statusline
-vim.opt.laststatus = 1
-
--- Neovim behavior
+-- Behavior
+vim.opt.clipboard = 'unnamedplus'
 vim.opt.confirm = true
+vim.opt.scrolloff = 8
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.timeoutlen = 500
-vim.opt.mouse = 'a'
-vim.opt.clipboard = 'unnamedplus'
 
 -- Indentation
 vim.opt.expandtab = true
-vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 
 -- Line wrapping
-vim.opt.linebreak = true
 vim.opt.breakindent = true
+vim.opt.linebreak = true
 
 -- Searching
 vim.opt.ignorecase = true
