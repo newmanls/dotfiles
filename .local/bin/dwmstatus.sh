74f8c684a60b6b08d5dbec5e76a3555714fde293
lr-tech <lrtech8@gmail.com>
2022-04-28 16:25:17 -0400

    Add `sb-cmus` module

--- .local/bin/dwmstatus.sh
+++ .local/bin/dwmstatus.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 while true; do
-    xsetroot -name " $(sb-volume)  $(sb-battery)  $(sb-network)  $(sb-date) "
+    xsetroot -name " $(sb-cmus)  $(sb-volume)  $(sb-battery)  $(sb-network)  $(sb-date) "
     sleep 0.5s 
 done
