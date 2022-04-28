7b68a59861c1616e04c7235d30ef80788cf18f0f
lr-tech <lrtech8@gmail.com>
2022-04-27 21:53:56 -0400

    Fix `nvim-treesitter` config

--- .config/nvim/plugin/nvim-treesitter.lua
+++ .config/nvim/plugin/nvim-treesitter.lua
@@ -1,9 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = "maintained",
-  sync_install = false,
-  highlight = {
-    enable = true,
-    --disable = { "c", "rust" },
-    additional_vim_regex_highlighting = false,
-  }
+    highlight = {
+        enable = true
+    }
 }
