343144989ac6aa5b0706420795b307dc1edb8e42
lr-tech <lrtech8@gmail.com>
2022-05-02 22:36:46 -0400

    Add `git a` alias to `git-setaliases.sh`

--- .local/bin/git-setaliases.sh
+++ .local/bin/git-setaliases.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git config --global alias.st 'status --short --branch'
-git config --global alias.add 'add --verbose'
+git config --global alias.a 'add --verbose'
 git config --global alias.cm 'commit -m'
 git config --global alias.co 'checkout'
 git config --global alias.br 'branch'
