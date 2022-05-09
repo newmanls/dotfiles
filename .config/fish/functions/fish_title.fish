21294cba56fbdc57caa37105a6f3eeff5c339a86
lr-tech <lrtech8@gmail.com>
2022-05-08 23:30:35 -0400

    Remove unused files

--- .config/fish/functions/fish_title.fish
+++
@@ -1,8 +0,0 @@
-function fish_title
-    # emacs' "term" is basically the only term that can't handle it.
-  if not set -q INSIDE_EMACS; or string match -vq '*,term:*' -- $INSIDE_EMACS
-        # An override for the current command is passed as the first parameter.
-    # This is used by `fg` to show the true process name, among others.
-    echo (set -q argv[1] && echo $argv[1] || status current-command) 
-    end
-end
