0e5cd0e76caaf6cfe5b56d9a59fc9a82f278fc08
newmanls <lrtech8@gmail.com>
2023-02-26 20:50:07 -0400

    update configs

--- .config/lf/rifle.sh
+++ .config/lf/rifle.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-set -Ceo pipefail
+set -C
 
 case "$(file --dereference --brief --mime-type -- "$f")" in
     text/*|application/json|inode/x-empty|application/pgp-encrypted)
         [ -z "$fs" ] && ${EDITOR} "$f" || ${EDITOR} -O $fx ;;
 
     *document*)
-        setsid -f zaread "$f" ;;
+        setsid -f zaread "$fx" > /dev/null 2>&1 ;;
 
     *)
-        setsid -f xdg-open "$f" > /dev/null 2>&1 ;;
+        setsid -f xdg-open "$fx" > /dev/null 2>&1 ;;
 esac
