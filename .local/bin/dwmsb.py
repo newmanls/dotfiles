874ec7d66a5779feb2a4672257b3ae3dbed94437
newmanls <newmansanchez189@gmail.com>
2023-03-02 23:31:39 -0400

    add cmus module to dwmsb.py

--- .local/bin/dwmsb.py
+++ .local/bin/dwmsb.py
@@ -2,9 +2,8 @@
 # dwm status script written in python
 # Author: Newman Sanchez (https://github.com/newmanls)
 
-import re
 from datetime import datetime
-from subprocess import run, PIPE
+from subprocess import run, getoutput
 from time import sleep
 
 LABELS = {
@@ -14,7 +13,25 @@ LABELS = {
     "network_wifi_off": "󰤭  ",
     "volume": "󰕾  ",
     "volume_muted": "󰸈  ",
+    "music": "  "
 }
+
+def cmus():
+    keys = ["status", "file", "tag title", "tag artist"]
+    info = {}
+
+    for line in getoutput("cmus-remote -Q").splitlines():
+        for key in keys:
+            if line.startswith(key + " "):
+                info[key.removeprefix("tag ")] = line.removeprefix(key).strip()
+
+    if info.get("status") and info["status"] != "stopped":
+        if info.get("title") and info.get("artist"):
+            output = "{} - {}".format(info["artist"], info["title"])
+        else:
+            output = info["file"].split("/")[-1]
+
+        return LABELS["music"] + output
 
 def date():
     date = datetime.now().strftime("%a %b %d %H:%M")
@@ -22,13 +39,12 @@ def date():
     return date
 
 def network():
-    cmd = ["nmcli", "-t", "-g", "NAME,TYPE", "c", "s", "--active"]
-    network = run(cmd, stdout=PIPE, text=True).stdout
+    network = getoutput("nmcli -t -g NAME,TYPE connection show --active")
     network_name, network_type = network.splitlines()[0].split(":")
 
-    if re.search("ethernet", network_type):
+    if network_type.endswith("ethernet"):
         label = LABELS["network_on"]
-    elif re.search("wireless", network_type):
+    elif network_type.endswith("wireless"):
         label = LABELS["network_wifi_on"]
     else:
         network_name = "disconnected"
@@ -37,27 +53,26 @@ def network():
     return label + network_name
 
 def pulseaudio():
-    cmd = ["pactl", "get-sink-mute", "0"]
-    muted = run(cmd, stdout=PIPE, text=True).stdout.split(":")[1].strip()
+    muted = getoutput("pactl get-sink-mute 0").split(":")[1].strip()
 
     if muted == "yes":
         volume = "muted"
         label = LABELS["volume_muted"]
     else:
-        cmd = ["pactl", "get-sink-volume", "0"]
-        volume = run(cmd, stdout=PIPE, text=True).stdout.split("/")[1].strip()
+        volume = getoutput("pactl get-sink-volume 0").split("/")[1].strip()
         label = LABELS["volume"]
 
     return label + volume
 
 
 if __name__ == "__main__":
-    DEFAULT_MODULES = [ "pulseaudio", "network", "date" ]
+    DEFAULT_MODULES = [ "cmus", "pulseaudio", "network", "date" ]
     SEPARATOR = "  "
 
     modules = DEFAULT_MODULES
 
     while True:
         status = [eval(module)() for module in modules]
+        status = [item for item in status if type(item) is str]
         run(["xsetroot", "-name", SEPARATOR.join(status)])
-        sleep(0.5)
+        sleep(0.25)
