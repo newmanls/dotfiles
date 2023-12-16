c88dada9f1b5a7e1cb51a4cf2b8a15db9b0a0eb1
newmanls <newmanls@pm.me>
2023-12-16 17:45:07 -0400

    Replace compile mapping for latex

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -1,9 +1,6 @@
-local map = vim.keymap.set
-
 vim.opt_local.colorcolumn = '0'
 vim.opt_local.shiftwidth = 2
 vim.opt_local.spell = true
 vim.opt_local.spelllang = 'es'
 
-map('n', '<leader>p', '<Cmd>wa | !latexmk -pdflua -pv<CR>', { buffer = true })
-map('n', '<F5>', '<Cmd>wa | !latexmk -pdflua<CR>', { buffer = true })
+vim.keymap.set('n', '<f5>', '<Plug>(vimtex-compile)')
