e5271e4e7d3a88340ae7a47bbf4ac09ccd66615f
Newman Sanchez <newmanls@pm.me>
2024-02-09 23:14:00 -0400

    added left/right mappings for mini.move

--- .config/nvim/plugin/mini.lua
+++ .config/nvim/plugin/mini.lua
@@ -32,8 +32,13 @@ require('mini.jump2d').setup({
 
 require('mini.move').setup({
     mappings = {
+        left = '<C-h>',
+        right = '<C-l>',
         down = '<C-j>',
         up = '<C-k>',
+
+        line_left = '<C-h>',
+        line_right = '<C-l>',
         line_down = '<C-j>',
         line_up = '<C-k>',
     },
