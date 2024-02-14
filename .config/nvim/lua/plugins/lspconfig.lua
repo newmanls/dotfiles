fed07d54ba3fdbe7c23c89be38c3291488c485bf
Newman Sanchez <newmanls@pm.me>
2024-02-13 23:49:16 -0400

    Added none-ls

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -27,18 +27,18 @@ return {
                             if not vim.loop.fs_stat(path .. '/.luarc.json') and
                                 not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
                                 client.config.settings = vim.tbl_deep_extend('force',
-                                client.config.settings, {
-                                    Lua = {
-                                        runtime = { version = 'LuaJIT' },
-                                        workspace = {
-                                            checkThirdParty = false,
-                                            library = { vim.env.VIMRUNTIME }
+                                    client.config.settings, {
+                                        Lua = {
+                                            runtime = { version = 'LuaJIT' },
+                                            workspace = {
+                                                checkThirdParty = false,
+                                                library = { vim.env.VIMRUNTIME }
+                                            }
                                         }
                                     }
-                                }
                                 )
 
-                                client.notify("workspace/didChangeConfiguration", {
+                                client.notify('workspace/didChangeConfiguration', {
                                     settings = client.config.settings
                                 })
                             end
@@ -52,11 +52,43 @@ return {
     {
         'neovim/nvim-lspconfig',
         keys = {
-            { 'K', vim.lsp.buf.hover },
-            { 'gd', vim.lsp.buf.definition },
+            { 'K',          vim.lsp.buf.hover },
+            { 'gd',         vim.lsp.buf.definition },
+            { '[d',         vim.diagnostic.goto_prev },
+            { ']d',         vim.diagnostic.goto_next },
             { '<leader>ca', vim.lsp.buf.code_action, mode = { 'n', 'v' } },
-            { '[d', vim.diagnostic.goto_prev },
-            { ']d', vim.diagnostic.goto_next },
         }
+    },
+    {
+        'nvimtools/none-ls.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+        event = 'VeryLazy',
+        keys = {
+            {
+                '<leader>cf',
+                function()
+                    vim.lsp.buf.format({ async = true })
+                end,
+                mode = { 'n', 'v' },
+            },
+        },
+        config = function()
+            local null_ls = require('null-ls')
+
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.autopep8,
+                    null_ls.builtins.formatting.isort,
+                },
+            })
+
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                callback = function()
+                    vim.lsp.buf.format({ async = false })
+                end,
+            })
+        end,
     }
 }
