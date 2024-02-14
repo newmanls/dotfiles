3998afa33aa42e9954b5a2a8213f2fbffa106561
Newman Sanchez <newmanls@pm.me>
2024-02-13 22:44:49 -0400

    Removed autopep

--- .config/nvim/lua/plugins/autopep8.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'tell-k/vim-autopep8',
-    ft = 'python',
-    lazy = true,
-    config = function()
-        vim.g.autopep8_disable_show_diff = 1
-        vim.g.autopep8_on_save = 1
-    end
-}
