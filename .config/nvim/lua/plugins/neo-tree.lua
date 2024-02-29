0e64ec2f7171e55db337ac13fa1b3b227715890a
Newman Sánchez <newmanls@pm.me>
2024-02-29 11:15:40 -0400

    Added options and mappings for neo-tree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -4,7 +4,25 @@ return {
     lazy = true,
     cmd = "Neotree",
     keys = {
-        { "<leader>ft", "<Cmd>Neotree toggle<CR>" },
+        { "<leader>tl", "<Cmd>Neotree left reveal<CR>" },
+        { "<leader>tt", "<Cmd>Neotree reveal toggle<CR>" },
+    },
+    opts = {
+        close_if_last_window = true,
+        enable_normal_mode_for_inputs = true,
+        sort_case_insensitive = true,
+        window = {
+            position = "float",
+            mappings = {
+                ["h"] = "close_node",
+                ["l"] = "open",
+                ["o"] = "add",
+                ["d"] = "cut_to_clipboard",
+                ["D"] = "delete",
+                ["<C-s>"] = "open_split",
+                ["<C-v>"] = "open_vsplit",
+            }
+        }
     },
     dependencies = {
         "nvim-lua/plenary.nvim",
