dcab44502fdac508e08225fe3d26d5c82fd79aaa
Newman Sánchez <newmanls@pm.me>
2024-03-07 10:02:22 -0400

    nvim: added nvim-silicon plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,26 +1,42 @@
 return {
     {
-        'jghauser/mkdir.nvim',
-        event = 'VeryLazy'
+        "jghauser/mkdir.nvim",
+        event = "VeryLazy"
     },
     {
-        'stevearc/oil.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        "stevearc/oil.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
         lazy = true,
-        cmd = 'Oil',
+        cmd = "Oil",
         keys = {
-            { '-', '<Cmd>Oil<CR>' }
+            { "-", "<Cmd>Oil<CR>" }
         },
         config = true
     },
     {
-        'folke/flash.nvim',
-        event = 'VeryLazy',
+        "folke/flash.nvim",
+        event = "VeryLazy",
         config = true
     },
     {
-        'mattn/emmet-vim',
+        "mattn/emmet-vim",
         lazy = true,
-        ft = { 'html', 'htmldjango', 'svelte' }
+        ft = { "html", "htmldjango", "svelte" }
+    },
+    {
+        "michaelrommel/nvim-silicon",
+        lazy = true,
+        cmd = "Silicon",
+        keys = {
+            { "<leader>sc", ":Silicon<CR>", mode = "v" }
+        },
+        opts = {
+            font = "Iosevka Nerd Font=24",
+            theme = "OneHalfDark",
+            output = function()
+                local save_dir = "~/Pictures/Screenshots/"
+                return save_dir .. os.date("!%Y%m%d_%H%M%S") .. "_code.png"
+            end,
+        }
     }
 }
