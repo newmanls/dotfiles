edc0348968798fa07e39e6fcfcc460d13bde8259
Newman Sánchez <newmanls@pm.me>
2024-03-08 14:25:15 -0400

    nvim: added fold options for tex

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -2,3 +2,5 @@ vim.opt_local.colorcolumn = '0'
 vim.opt_local.shiftwidth = 2
 vim.opt_local.spell = true
 vim.opt_local.spelllang = 'es'
+vim.opt_local.foldmethod = 'marker'
+vim.opt_local.foldmarker = '<<<,>>>'
