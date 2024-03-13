4fc9b2e9abbbc120a4a9e78d8f233d6147a2a17d
Newman Sánchez <newmanls@pm.me>
2024-03-13 15:52:24 -0400

    nvim: added nvim-treesitter-context plugin

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,16 +2,18 @@ return {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPost", "BufWritePost", "BufNewFile" },
     build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "bash", "lua", "python" },
-            auto_install = true,
-            highlight = {
-                enable = true
-            },
-            incremental_selection = {
-                enable = true,
-            },
-        })
-    end
+    main = "nvim-treesitter.configs",
+    opts = {
+        ensure_installed = { "bash", "lua", "python", "javascript" },
+        auto_install = true,
+        highlight = {
+            enable = true
+        },
+        incremental_selection = {
+            enable = true,
+        },
+    },
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-context"
+    }
 }
