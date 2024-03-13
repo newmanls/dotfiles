b699863a47fcc84c1d770a77a699b782ee0ec675
Newman Sanchez <newmanls@pm.me>
2024-03-12 23:05:24 -0400

    nvim: update load event for treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    event = "VeryLazy",
+    event = { "BufReadPre", "BufNewFile" },
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
