3d830ac990e77afad78bf63096034459c1adf304
Newman Sanchez <newmanls@pm.me>
2024-05-12 11:05:08 -0400

    nvim: ensure treesitter parsers for markdown are installed

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,14 @@ return {
     build = ":TSUpdate",
     main = "nvim-treesitter.configs",
     opts = {
-        ensure_installed = { "bash", "lua", "python", "javascript" },
+        ensure_installed = {
+            "bash",
+            "lua",
+            "python",
+            "javascript",
+            "markdown",
+            "markdown_inline",
+        },
         auto_install = true,
         highlight = {
             enable = true
