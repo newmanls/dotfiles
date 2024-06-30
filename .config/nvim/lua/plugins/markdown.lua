186e30b18462b44844ccd55958de46ffb46e15c7
Newman Sanchez <newmanls@pm.me>
2024-06-30 09:56:21 -0400

    nvim: add render-markdown plugin

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -46,5 +46,22 @@ return {
         opts = {
             app = "browser"
         }
+    },
+    {
+        "MeanderingProgrammer/markdown.nvim",
+        name = "render-markdown",
+        ft = "markdown",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            headings = { "⦿ " },
+            bullets = { "•", "◦" },
+            checkbox = {
+                unchecked = "☐ ",
+                checked = "☑ ",
+            }
+        },
     }
 }
