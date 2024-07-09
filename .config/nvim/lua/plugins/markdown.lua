9d53f028192db08b3e1f091df95d33aa8d5f3ffc
Newman Sanchez <newmanls@pm.me>
2024-07-08 21:58:37 -0400

    nvim: replace markdown.nvim with bullets.vim

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,39 +1,7 @@
 return {
     {
-        "tadmccorkle/markdown.nvim",
-        opts = {},
+        "bullets-vim/bullets.vim",
         ft = "markdown",
-        keys = {
-            {
-                "gtt",
-                "<Cmd>MDTaskToggle<CR>",
-                desc = "Toggle task on the current cursor line"
-            },
-            {
-                "gt",
-                ":MDTaskToggle<CR>",
-                silent = true,
-                mode = "v",
-                desc = "Toggle tasks under selected lines"
-            },
-            {
-                "<M-o>",
-                "<Cmd>MDListItemBelow<CR>",
-                mode = { "n", "i" },
-                desc = "Add new list item in the next line"
-            },
-            {
-                "<M-O>",
-                "<Cmd>MDListItemAbove<CR>",
-                mode = { "n", "i" },
-                desc = "Add new list item in the previous line"
-            },
-            {
-                "<leader>mt",
-                "<Cmd>MDToc<CR>",
-                desc = "Open table of contents"
-            }
-        },
     },
     {
         "toppair/peek.nvim",
