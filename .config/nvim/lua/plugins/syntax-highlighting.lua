6db5a398b9a23528518f18a2ad7ad11cedf40109
Newman Sánchez <newmanls@pm.me>
2024-07-22 15:32:04 -0400

    nvim: disable diagnostic text highlight for gruvbox-material colorscheme

--- .config/nvim/lua/plugins/syntax-highlighting.lua
+++ .config/nvim/lua/plugins/syntax-highlighting.lua
@@ -25,7 +25,6 @@ return {
         init = function()
             vim.g.gruvbox_material_background = "hard"
             vim.g.gruvbox_material_enable_bold = true
-            vim.g.gruvbox_material_diagnostic_text_highlight = true
             vim.g.gruvbox_material_better_performance = true
         end
     },
