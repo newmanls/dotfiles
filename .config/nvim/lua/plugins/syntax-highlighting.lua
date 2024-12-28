4935d2ff9bcf1a1dda5e857e2ade245246e6344f
Newman Sanchez <newmanls@pm.me>
2024-12-28 01:02:14 -0400

    refactor(nvim): move colorscheme plugins to their own file

--- .config/nvim/lua/plugins/syntax-highlighting.lua
+++ .config/nvim/lua/plugins/syntax-highlighting.lua
@@ -20,19 +20,4 @@ return {
         "brenoprata10/nvim-highlight-colors",
         opts = {}
     },
-    {
-        "sainnhe/gruvbox-material",
-        init = function()
-            vim.g.gruvbox_material_background = "hard"
-            vim.g.gruvbox_material_enable_bold = true
-            vim.g.gruvbox_material_better_performance = true
-        end
-    },
-    {
-        "navarasu/onedark.nvim",
-        priority = 1000,
-        opts = {
-            style = "warmer"
-        }
-    }
 }
