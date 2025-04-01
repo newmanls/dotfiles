d4ec56ded4099ec1f627fa1888bc468dc26de3d7
Newman Sánchez <newmanls@pm.me>
2025-04-01 12:20:14 -0400

    feat(nvim): Remove custom lsp keymappings
    Remove the custom lsp keymappings since there are default ones in v0.11
    
    | Action            | Old       | New           |
    | ---               | ---       | ---           |
    | Symbol info       | K         | K (No change) |
    | Go to definition  | gd        | C-]           |
    | List code actions | crr       | gra           |
    | Rename symbol     | crn       | grn           |
    | Format buffer     | <leader>= | gq            |

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -9,34 +9,6 @@ return {
             })
         end,
         event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-        keys = {
-            {
-                "K",
-                vim.lsp.buf.hover,
-                desc = "Display information about the symbol under the cursor"
-            },
-            {
-                "gd",
-                vim.lsp.buf.definition,
-                desc = "Go to definition"
-            },
-            {
-                "crr",
-                vim.lsp.buf.code_action,
-                mode = { "n", "v" },
-                desc = "List available code actions"
-            },
-            {
-                "crn",
-                vim.lsp.buf.rename,
-                desc = "Renames all references to the symbol under the cursor"
-            },
-            {
-                "<leader>=",
-                vim.lsp.buf.format,
-                desc = "Formats a buffer using the attached language server"
-            },
-        },
         dependencies = {
             "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
