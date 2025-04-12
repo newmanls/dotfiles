74142199857103b1267b44425a1336ef2a7b6e0e
Newman Sanchez <newmanls@pm.me>
2025-04-12 10:43:43 -0400

    fix(nvim): Make diagnostic float rounded and open on cursor hold after 500ms

--- .config/nvim/plugin/00_options.lua
+++ .config/nvim/plugin/00_options.lua
@@ -9,6 +9,7 @@ vim.opt.termguicolors = true
 -- Behavior
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.confirm = true
+vim.opt.updatetime = 500
 vim.opt.scrolloff = 4
 vim.opt.splitright = true
 vim.opt.splitbelow = true
@@ -29,16 +30,17 @@ vim.opt.smartcase = true
 -- Diagnostics
 vim.diagnostic.config({
     severity_sort = true,
-    jump = { float = true },
+    float = { border = 'rounded' },
+})
+
+-- Open floating diagnostic on cursor move
+vim.api.nvim_create_autocmd('CursorHold', {
+    callback = function()
+        vim.diagnostic.open_float(nil, { focus = false })
+    end
 })
 
 -- Autocmds
 vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function() vim.highlight.on_yank() end
-})
-
-vim.api.nvim_create_autocmd('CursorHold', {
-    callback = function()
-        vim.diagnostic.open_float(nil, { focus = false })
-    end
 })
