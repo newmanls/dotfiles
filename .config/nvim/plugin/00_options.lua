fee21b091b1ca60481a9cad4951a5736fc962343
Newman Sanchez <newmanls@pm.me>
2025-05-18 21:03:39 -0400

    feat(nvim): Disable relativenumber on insert mode

--- .config/nvim/plugin/00_options.lua
+++ .config/nvim/plugin/00_options.lua
@@ -46,3 +46,15 @@ vim.api.nvim_create_autocmd('CursorHold', {
 vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function() vim.highlight.on_yank() end
 })
+
+vim.api.nvim_create_autocmd('InsertEnter', {
+    callback = function ()
+        vim.opt_local.relativenumber=false
+    end
+})
+
+vim.api.nvim_create_autocmd('InsertLeave', {
+    callback = function ()
+        vim.opt_local.relativenumber=true
+    end
+})
