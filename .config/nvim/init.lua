e9d7fc23af3f6b559ea9ebfe4dfede02a2c592d2
Newman Sanchez <newmanls@pm.me>
2025-11-06 23:06:05 -0400

    feat(nvim): Add autocmds to automatically resize splits and to restore cursor position on file open

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,7 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.syntax = 'on'
 vim.o.termguicolors = true
+vim.o.formatoptions = 'jcrql'
 
 -- Behavior
 vim.o.clipboard = 'unnamedplus'
@@ -52,10 +53,24 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function() vim.highlight.on_yank() end
 })
 
--- Open help in vertical split
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'help',
-    command = 'wincmd L',
+-- restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function(args)
+        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+        local line_count = vim.api.nvim_buf_line_count(args.buf)
+        if mark[1] > 0 and mark[1] <= line_count then
+            vim.api.nvim_win_set_cursor(0, mark)
+            -- defer centering slightly so it's applied after render
+            vim.schedule(function()
+                vim.cmd("normal! zz")
+            end)
+        end
+    end,
+})
+
+-- automatically resize splits when the terminal window is resized
+vim.api.nvim_create_autocmd('VimResized', {
+    command = 'wincmd ='
 })
 
 
