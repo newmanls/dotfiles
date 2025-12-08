5e442a8985e5a141b51f0dfac1e34b46ada390d0
Newman Sanchez <newmanls@pm.me>
2025-12-07 23:25:03 -0400

    feat(nvim): Add wrapping on markdown files

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,5 +1,6 @@
 vim.opt_local.colorcolumn = '0'
 vim.opt_local.spell = true
+vim.opt_local.wrap = true
 vim.opt_local.spelllang = 'es,en'
 
 -- Add bold, italics and link surrounds to 'mini.surround'
