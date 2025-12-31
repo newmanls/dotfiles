cc2187436dd9673e6110cc4357a3af63b5831792
Newman Sanchez <newmanls@pm.me>
2025-12-30 22:37:21 -0400

    feat(matugen): Use custom colors for nvim statusline and cursor line number

--- .config/matugen/templates/nvim_mini_hues.lua
+++ .config/matugen/templates/nvim_mini_hues.lua
@@ -1,4 +1,13 @@
 require('mini.hues').setup({
     background = '{{ colors.surface_container.default.hex }}',
     foreground = '{{ colors.on_surface.default.hex }}',
+    accent = 'fg',
+})
+
+vim.api.nvim_set_hl(0, 'MiniStatuslineDevinfo', {
+    bg = '{{ colors.primary_container.default.hex }}',
+})
+
+vim.api.nvim_set_hl(0, 'CursorLineNr', {
+    fg = '{{ colors.secondary.default.hex }}',
 })
