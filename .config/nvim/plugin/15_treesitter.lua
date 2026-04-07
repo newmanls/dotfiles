2d3c41c6cc9b2adcdc4e354a499ce66c4e0fe31b
Newman Sánchez <newmanls@pm.me>
2026-04-07 14:45:20 -0400

    fix: Ensure treesitter is using the main branch

--- .config/nvim/plugin/15_treesitter.lua
+++ .config/nvim/plugin/15_treesitter.lua
@@ -9,7 +9,10 @@ vim.api.nvim_create_autocmd('PackChanged', {
 })
 
 vim.pack.add({
-    'https://github.com/nvim-treesitter/nvim-treesitter',
+    {
+        src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+        version = 'main',
+    },
     'https://github.com/nvim-treesitter/nvim-treesitter-context',
     'https://github.com/MeanderingProgrammer/treesitter-modules.nvim',
 })
