e1de3e2733a169445b24febf1b5b4d829b21ba99
Newman Sánchez <newmanls@pm.me>
2026-04-21 21:25:27 -0400

    fix(nvim): Configure lua_ls for neovim

--- .config/nvim/after/lsp/lua_ls.lua
+++ .config/nvim/after/lsp/lua_ls.lua
@@ -1,9 +1,22 @@
 return {
     settings = {
         Lua = {
+            runtime = {
+                version = 'LuaJIT',
+                path = vim.split(package.path, ';')
+            },
+            diagnostics = {
+                -- Don't analyze whole workspace, as it consumes too much CPU
+                -- and RAM
+                workspaceDelay = -1,
+            },
             workspace = {
-                library = vim.api.nvim_get_runtime_file('', true),
-            }
+                -- Don't analyze code from submodules
+                ignoreSubmodules = true,
+                -- Add Neovim's methods for easier code writing
+                library = { vim.env.VIMRUNTIME },
+            },
+            telemetry = { enable = false },
         }
     }
 }
